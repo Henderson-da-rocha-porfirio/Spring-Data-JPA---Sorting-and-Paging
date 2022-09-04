3fcf4acb340ec08e6f250b4cd7477f33b59efaf3
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-09-04 17:02:57 -0300

    #Commit

--- src/main/java/com/tuyo/produtodata/produtosdata/ProdutosdataSortingPagingApplication.java
+++ src/main/java/com/tuyo/produtodata/produtosdata/ProdutosdataSortingPagingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProdutosdataApplication {
+public class ProdutosdataSortingPagingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProdutosdataApplication.class, args);
+		SpringApplication.run(ProdutosdataSortingPagingApplication.class, args);
 	}
 
 }
